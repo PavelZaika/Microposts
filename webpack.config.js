43dc47ac7423d3bd1d9d6e5a360a9153231b918f
Loki <zpk.loki@gmail.com>
2019-12-03 18:26:18 +0300

    education part

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   entry: {
-    app: ['@babel/polyfill', './src/app.js']
+    app: ['@babel/polyfill', './src/mainApp.js']
   },
   output: {
     path: path.resolve(__dirname, 'build'),
