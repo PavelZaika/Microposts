8e7d352b1a6e5ea0b28b7e88b501c59b37ad9f1c
Loki <zpk.loki@gmail.com>
2019-12-06 17:54:49 +0300

    Get and display posts

---
+++ src/http.js
@@ -0,0 +1,51 @@
+class EasyHTTP {
+  // Make an http get request
+  async get(url) {
+    const response = await fetch(url);
+
+    const resData = await response.json();
+    return resData;
+  }
+
+  // Make an http post request
+  async post(url, data) {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify(data)
+    });
+
+    const resData = await response.json();
+    return resData;
+  }
+  // Make an http PUT request
+  async put(url, data) {
+    const response = await fetch(url, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify(data)
+    });
+    const resData = await response.json();
+    return resData;
+  }
+
+  // Make an http delete request
+
+  async delete(url) {
+    const response = await fetch(url, {
+      method: "DELETE",
+      headers: {
+        "Content-type": "application/json"
+      }
+    });
+
+    const resData = await "Resource deleted";
+    return resData;
+  }
+}
+
+export const http = new EasyHTTP();
