60a22f6028c7d37c540a980ae30a3e7e3fd1f3a8
Loki <zpk.loki@gmail.com>
2019-12-09 13:58:24 +0300

    add posts and show alert

--- src/app.js
+++ src/app.js
@@ -2,12 +2,38 @@ import { http } from "./http";
 import { ui } from "./ui";
 
 // get posts on DOM load
-
 document.addEventListener("DOMContentLoaded", getPosts);
 
+//Listen for add post
+document.querySelector(".post-submit").addEventListener("click", submitPost);
+
+
+// Get posts
 function getPosts() {
   http
     .get("http://localhost:3000/posts")
     .then(data => ui.showPost(data))
     .catch(err => console.log(err));
+}
+
+//Submit post
+function submitPost() {
+  const title = document.querySelector('#title').value;
+  const body = document.querySelector('#body').value;
+
+  const data = {
+    title: title,
+    body: body
+  };
+
+  // Create Post
+  http.post("http://localhost:3000/posts", data)
+    .then(data => {
+      ui.showAlert('Post added', 'alert alert-success');
+      ui.clearField();
+      getPosts();
+    })
+    .catch(err => console.log(err));
+    
+    
 }
